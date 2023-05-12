ff2c337e3baf392c6094a345839f961849648e29
asleulv <101048079+asleulv@users.noreply.github.com>
2023-05-12 15:27:11 +0200

    Added more levels

--- app/quiz_settings.py
+++ app/quiz_settings.py
@@ -5,5 +5,8 @@ levels = {
 			4:{'peak': 1, 'weeks': 15, 'f_range': 1960,'t_range': 2010},
             5:{'peak': 1, 'weeks': 10, 'f_range': 1958,'t_range': 2020},
             6:{'peak': 2, 'weeks': 10, 'f_range': 1958,'t_range': 2020},
-            7:{'peak': 3, 'weeks': 5, 'f_range': 1958,'t_range': 2020}
+            7:{'peak': 3, 'weeks': 5, 'f_range': 1958,'t_range': 2020},
+            8:{'peak': 4, 'weeks': 5, 'f_range': 1958,'t_range': 2020},
+            9:{'peak': 5, 'weeks': 3, 'f_range': 1958,'t_range': 2020},
+            10:{'peak': 6, 'weeks': 3, 'f_range': 1958,'t_range': 2020}
 		  }
