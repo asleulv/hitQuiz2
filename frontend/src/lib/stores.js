6d3c19c776abf7b3ac6d8e28c35a33f833a62b46
origin-unknown <ich.hier.wer.dort@gmx.de>
2023-05-17 23:50:12 +0200

    Change of colors more random.

---
+++ frontend/src/lib/stores.js
@@ -0,0 +1,13 @@
+import { writable } from 'svelte/store';
+
+/* Colors used:
+ * #144bb8
+ * #7e14b8
+ * #b8144d
+ * #b87e14
+ * #4db814
+ * #14b87e
+*/
+export const background = writable('#14b87e'); 
+
+
