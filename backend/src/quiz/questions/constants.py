28e694a122ef81755903d392f88e9ce9f497b6b9
asleulv <101048079+asleulv@users.noreply.github.com>
2023-05-24 22:17:05 +0200

    Changed intro text

--- backend/src/quiz/questions/constants.py
+++ backend/src/quiz/questions/constants.py
@@ -1,7 +1,7 @@
 LEVELS = {
-	1: {'peak': 1, 'weeks': 30, 'f_range': 1980,'t_range': 1999},
-	2: {'peak': 1, 'weeks': 25, 'f_range': 1975,'t_range': 2005},
-	3: {'peak': 1, 'weeks': 20, 'f_range': 1965,'t_range': 2005},
+	1: {'peak': 1, 'weeks': 25, 'f_range': 1980,'t_range': 1999},
+	2: {'peak': 1, 'weeks': 20, 'f_range': 1975,'t_range': 2005},
+	3: {'peak': 1, 'weeks': 15, 'f_range': 1965,'t_range': 2005},
 	4: {'peak': 1, 'weeks': 15, 'f_range': 1960,'t_range': 2010},
 	5: {'peak': 1, 'weeks': 10, 'f_range': 1958,'t_range': 2020},
 	6: {'peak': 2, 'weeks': 10, 'f_range': 1958,'t_range': 2020},
