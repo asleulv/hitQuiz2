503e18deef4cb3bad2566f611e04229168a27f36
asleulv <101048079+asleulv@users.noreply.github.com>
2023-05-31 10:39:52 +0200

    Update routes.py: Altnernatives filter

--- backend/src/quiz/questions/routes.py
+++ backend/src/quiz/questions/routes.py
@@ -129,8 +129,8 @@ def update():
 		while len(alternatives) < 4:
 			alt = Hit.query\
 				.filter(
-					Hit.peak == LEVELS[level_key]['peak'], 
-					Hit.weeks > LEVELS[level_key]['weeks'], 
+					Hit.peak <= LEVELS[level_key]['peak'], 
+					Hit.weeks >= LEVELS[level_key]['weeks'], 
 					Hit.year.in_(range(LEVELS[level_key]['f_range'],LEVELS[level_key]['t_range'])), 
 					Hit.artist.not_in(alternatives), 
 					# Hit.id.not_in(session['seen_songs']) # Why should this limitation exist?
