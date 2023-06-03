54b86ff3fdda5398d7e219d2dc7d7c7e0a811180
origin-unknown <ich.hier.wer.dort@gmx.de>
2023-06-03 17:54:59 +0200

    Open Graph protocol

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://hitQuiz.me/" />
 	<meta property="og:image" content="https://hitQuiz.me/poster.png" />
+	<meta property="og:image:type" content="image/png" />
+	<meta property="og:image:width" content="768" />
+	<meta property="og:image:height" content="432" />
 	<meta 
 		property="og:description" 
 		content="Given the song title, year and peak position the question remains exactly that…" 
