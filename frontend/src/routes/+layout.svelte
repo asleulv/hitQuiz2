b1c854d0692daabe0724c755872532bf36f91dcb
origin-unknown <ich.hier.wer.dort@gmx.de>
2023-06-03 17:49:53 +0200

    Open Graph protocol

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+
+<svelte:head>
+	<meta property="og:title" content="hitQuiz.me - Who had the hit?" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://hitQuiz.me/" />
+	<meta property="og:image" content="https://hitQuiz.me/poster.png" />
+	<meta 
+		property="og:description" 
+		content="Given the song title, year and peak position the question remains exactly that…" 
+	/>
+</svelte:head>
+
+<slot />
