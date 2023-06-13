c8eb9923564a71a2db121f7e5142eadc6f3a29d9
origin-unknown <ich.hier.wer.dort@gmx.de>
2023-06-13 15:44:07 +0200

    Migration order changed

--- backend/migrations/versions/956853b54a2d_.py
+++ backend/migrations/versions/956853b54a2d_.py
@@ -6,13 +6,14 @@ Create Date: 2023-05-15 23:48:23.584543
 
 """
 from alembic import op
+from quiz import db
 from quiz.questions.models import Hit
 import sqlalchemy as sa
 import json
 
 # revision identifiers, used by Alembic.
 revision = '956853b54a2d'
-down_revision = '5693a41970f2'
+down_revision = '4b8d0ecfe74b' 
 branch_labels = None
 depends_on = None
 
@@ -24,4 +25,9 @@ def upgrade():
 
 
 def downgrade():
+    try:
+        Hit.query.delete()
+        db.session.commit()
+    except: 
+        db.session.rollback()
     pass
