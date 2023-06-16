c2acc5d2cf54578f6457ce1e54bf8adcdf31031c
origin-unknown <ich.hier.wer.dort@gmx.de>
2023-06-16 16:19:10 +0200

    comma-separated duplicates are removed

--- backend/src/quiz/questions/utils.py
+++ backend/src/quiz/questions/utils.py
@@ -1,10 +1,19 @@
+import re
 
 def find_parts(t, p):
 	delim = ' '
-	tokens = t.split(delim)
+	tokens = re.split(r'[,\s]', t) 
 	size = len(tokens)
+	plen = len(p)
 	for i in range(size):
 		for j in range(i + 1, size + 1):
-			if p == delim.join(tokens[i:j]):
-				return True
+			_t = delim.join(tokens[i:j])
+			_tlen = len(_t)
+			if _tlen > plen:
+				break
+			elif _tlen < plen:
+				continue
+			else: 
+				if _t == p:
+					return True
 	return False
